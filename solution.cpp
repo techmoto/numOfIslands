d94418499b95d4d11e6b9d4b7a8ddefde32a43f5
techmoto <103876700+techmoto@users.noreply.github.com>
2022-12-16 23:36:57 -0800

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -1,5 +1,8 @@
 class Solution {
+    
 public:
+    // returns the number of islands. What determines an island if vertex value '1' in the grid has edges
+    // to another vertex '1'
     int numIslands(vector<vector<char>>& grid) {
         
         int numIslands = 0; // counter variable to sum the number of islands
@@ -9,13 +12,15 @@ public:
             for(int c = 0; c < grid.size(); c++){ // colmuns loop
                 if(grid[r][c] == '1'){
                     numIslands++;  // incremeants the number of islands
-                    BFS(grid,r,c); // Breadth Search Algorithnm 
+                    BFS(grid,r,c); // Breadth Search Algorithnm, checks for all vertices to see if it has edges
                 }
             }
         }
-        return numIslands;   
+        return numIslands; //once the for loops finish iterating, it will return the number of islands.
     }
+ 
 private:
+    // private helper function
     void BFS(int vtx, vector<vector<char>> &iGraph, int gSize) {
         queue<int> Q;
         int* visited;
